c5e294277124f84953513a58233009a8db3cd29d
Risitop <aziz.foucheasnoun@gmail.com>
2018-06-12 15:44:29 +0200

    CADD correct normalization

--- Visualizer_preparation/preparation.py
+++ Visualizer_preparation/preparation.py
@@ -11,22 +11,27 @@ features = 'CADD1,CADD2,RecA,EssA,CADD3,CADD4,RecB,EssB,Path'.split(',')
 df_data = pd.read_csv("dida_posey_to_predict.csv")
 df_data.head()
 
-combination = sys.argv[1]
+combination = list(map(int, sys.argv[1]))
+n_comb = sum(combination)
 X = array(df_data[features])
-X = X[ :,[c == '1' for c in combination] ]
+X = dot(X, diag(combination))
 
-X = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
+X[:,[0,1,4,5]] -= X[:,[0,1,4,5]].min()
+X[:,[0,1,4,5]] /= X[:,[0,1,4,5]].max()
 
-X_transform = TSNE(n_components=2, init="pca").fit_transform(X)
-X_transform = (X_transform - X_transform.min(axis=0)) / (X_transform.max(axis=0) - X_transform.min(axis=0))
-X_transform = nan_to_num(X_transform)
+if n_comb > 2:
+    X = TSNE(n_components=2, init="pca").fit_transform(X)
+    X = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
+    X = nan_to_num(X)
+else:
+    X = X[:, combination]
 
 df_data_vs = df_data.copy(False)
-df_data_vs['x'] = X_transform[:,0]
-df_data_vs['y'] = X_transform[:,1]
+df_data_vs['x'] = X[:,0]
+df_data_vs['y'] = X[:,1] if len(X.T) > 1 else 0
 df_data_vs = df_data_vs.drop('Pair', 1)
 
-with open("exports/p_file_" + combination + ".csv", "w") as out:
+with open("exports/p_file_" + ''.join(map(str, combination)) + ".csv", "w") as out:
     out.write('id,x,y\n')
     for line in array(df_data_vs):
         out.write(','.join(map(str, line[[0,-2,-1]])) + '\n')
